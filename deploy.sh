a95b7e9ce536b9d5eb258fecc4d8ddc4ccd91014
diwu1990 <diwu0111@hotmail.com>
2021-01-20 03:05:21 -0600

    add update

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-git add *
-git commit -m "add update"
-git push
-
 # Clean up the public dir
 if [ -d "public" ]
 then
     rm -rf public
 fi
 
-git submodule add -f -b master https://github.com/diwu1990/diwu1990.github.io.git public
+git add *
+git commit -m "add update"
+git push
+
+git submodule add -f -b main https://github.com/diwu1990/diwu1990.github.io.git public
 cd public
 git checkout main
 cd ..
